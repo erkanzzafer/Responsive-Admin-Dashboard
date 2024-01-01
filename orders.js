4cd5cd1a86e3c62ac52db5d7d414c0364705adf4
erkanzzafer <zzafererkann@gmail.com>
2024-01-01 23:41:20 +0300

    content completed

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders=[
+    {
+        productName : 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }, {
+        productName : 'Foldable Mini Drone1',
+        productNumber: '856311',
+        paymentStatus: 'Due1',
+        shipping: 'Pending'
+    }, {
+        productName : 'Foldable Mini Drone2',
+        productNumber: '856312',
+        paymentStatus: 'Due2',
+        shipping: 'Pending'
+    }, {
+        productName : 'Foldable Mini Drone3',
+        productNumber: '856313',
+        paymentStatus: 'Due3',
+        shipping: 'Declined'
+    }, {
+        productName : 'Foldable Mini Drone4',
+        productNumber: '856314',
+        paymentStatus: 'Due4',
+        shipping: 'Declined'
+    }, {
+        productName : 'Foldable Mini Drone5',
+        productNumber: '856315',
+        paymentStatus: 'Due5',
+        shipping: 'Declined'
+    }, {
+        productName : 'Foldable Mini Drone6',
+        productNumber: '856316',
+        paymentStatus: 'Due6',
+        shipping: 'Declined'
+    },
+];
